c50f7e636527577eafc4c1ffd8a9af6509d29ef2
jeremys18 <jeremys18@gmail.com>
2025-03-05 01:10:00 -0500

    consts

--- BeatSaberDownloader.Data/Consts/BeatSaverConsts.cs
+++ BeatSaberDownloader.Data/Consts/BeatSaverConsts.cs
@@ -3,8 +3,13 @@
     public class BeatSaverConsts
     {
         public const string BeatSaverAPIBaseURL = "https://api.beatsaver.com";
+
+        // Maps
         public const string BeatSaverMapSearchURL = "maps/latest?after={after}&automapper=true&pageSize={pagesize}"; // We want this with the time of the last map in the list of the current page. If first page then make it the date beat saber was released
         public const int BeatSaverMapRequestPageSize = 100; // Current max
-        public const string DefaultBeatSaverAfterDate = "2018-05-01"; // May 1st 2018. Beat Saber released
+        public const string DefaultBeatSaverAfterDate = "2018-05-01"; // May 1st 2018. Beat Saber
+                                                                      
+        // Playlists
+        public const string BeatSaverMapSearchURL = "playlists/latest?after={after}&automapper=true&pageSize={pagesize}";
     }
 }
