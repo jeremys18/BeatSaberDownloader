ae59b324e14b0b444474de462fbd3cd0406f02f0
Jeremy Styers <jeremys18@gmail.com>
2025-12-26 07:28:28 -0600

    Add ldb logging to services

--- BeatSaberDownloader.DownloadService/Program.cs
+++ BeatSaberDownloader.DownloadService/Program.cs
@@ -1,8 +1,32 @@
 using BeatSaberDownloader.DownloadService;
 using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
+
+var connectionString = Environment.GetEnvironmentVariable("DBDOWNLOADER_SERVICE_LOG_DB") ?? "Server=.;Database=BeatSaberDownloader;Trusted_Connection=True;TrustServerCertificate=True;";
+
+var sinkOptions = new MSSqlServerSinkOptions
+{
+    SchemaName = "DownloadService",
+    TableName = "Logs",
+    AutoCreateSqlTable = true
+};
+
+var columnOptions = new ColumnOptions
+{
+    TimeStamp = { ColumnName = "LoggedAt", ConvertToUtc = true }
+};
 
 Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
     .WriteTo.File(@"G:\BeatSaber\Logs\SongDownloaderSrv.log", rollingInterval: RollingInterval.Day)
+    .WriteTo.MSSqlServer(
+        connectionString: connectionString,
+        sinkOptions: sinkOptions,
+        columnOptions: columnOptions,
+        restrictedToMinimumLevel: LogEventLevel.Information)
     .CreateLogger();
 
 var builder = Host.CreateApplicationBuilder(args);
