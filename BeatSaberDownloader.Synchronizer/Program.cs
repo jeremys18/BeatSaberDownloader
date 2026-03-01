8521fb6b73776f752262529c73829789cf16a9f6
jeremys18 <jeremys18@gmail.com>
2026-02-28 21:05:07 -0600

    Add sync Service to handle syncing missing songs/updates

---
+++ BeatSaberDownloader.Synchronizer/Program.cs
@@ -0,0 +1,43 @@
+using BeatSaberDownloader.Synchronizer;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
+
+var connectionString = Environment.GetEnvironmentVariable("DBDOWNLOADER_SYNCSERVICE_LOG_DB") ?? "Server=.;Database=BeatSaberDownloader;Trusted_Connection=True;TrustServerCertificate=True;";
+
+var sinkOptions = new MSSqlServerSinkOptions
+{
+    SchemaName = "SyncService",
+    TableName = "Logs",
+    AutoCreateSqlTable = true
+};
+
+var columnOptions = new ColumnOptions
+{
+    TimeStamp = { ColumnName = "LoggedAt", ConvertToUtc = true }
+};
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.File(@"G:\BeatSaber\Logs\SyncSrv.log", rollingInterval: RollingInterval.Day)
+    .WriteTo.MSSqlServer(
+        connectionString: connectionString,
+        sinkOptions: sinkOptions,
+        columnOptions: columnOptions,
+        restrictedToMinimumLevel: LogEventLevel.Information)
+    .CreateLogger();
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = "BeatSaber Song Sync Service";
+});
+builder.Services.AddHostedService<Worker>();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog();
+builder.Logging.AddConsole();
+
+var host = builder.Build();
+host.Run();
